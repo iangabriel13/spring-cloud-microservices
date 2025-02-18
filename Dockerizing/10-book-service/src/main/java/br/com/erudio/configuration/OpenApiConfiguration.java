596ea73b5a85ea2ab3464f2adc7a6a67b07ac5e1
Ian Gabriel <42757211+iangabriel13@users.noreply.github.com>
2025-02-18 20:05:39 +0000

    Fixing swagger

--- Dockerizing/10-book-service/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
+++ Dockerizing/10-book-service/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
@@ -2,11 +2,11 @@ package br.com.erudio.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 @OpenAPIDefinition(info =
@@ -22,12 +22,7 @@ public class OpenApiConfiguration {
 			.info(
 				new io.swagger.v3.oas.models.info.Info()
 				.title("Book Service API")
-				.version("v1")
-				.license(
-					new License()
-						.name("Apache 2.0")
-						.url("http://springdoc.org")
-				)
-			);
+                .version("1.0")
+                .description("Documentation of Book Service API"));
 	}
 }
